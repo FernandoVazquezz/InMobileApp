41233236da9db9a8cd42a3bed7c402f26e14be2b
unknown <luisfernando.flores@uttehuacan.edu.mx>
2020-02-19 18:09:46 -0600

    Iconos

--- modules/initial/SignUp.js
+++ modules/initial/SignUp.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class SigSignUpnIn extends React.Component {
   
@@ -9,7 +10,7 @@ export default class SigSignUpnIn extends React.Component {
       return (  
           <View>
               <Text >
-               SignUp
+              <Icon name="comments" size={30} color="#900" />
               </Text>
           </View>
       );
